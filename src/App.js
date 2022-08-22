8b6d79da1edcaf70d12925d09808da16815355a8
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-08-22 13:05:00 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by{" "}
+        <a href="https://tiny-semolina-5cec1e.netlify.app/" target="_blank">
+          Nataliia Prokopenko{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/NataliiaProkopenko87/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
