507f889271640f64ed8f90feb7dddb54369fb904
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-08-22 14:21:51 +0200

    Added footer and change project name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://tiny-semolina-5cec1e.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nataliia Prokopenko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/NataliiaProkopenko87/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
