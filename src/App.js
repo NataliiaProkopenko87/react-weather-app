1da027263c31243b4c70dfbd9e9ab805f5d368c2
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-08-22 14:41:58 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://tiny-semolina-5cec1e.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nataliia Prokopenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/NataliiaProkopenko87/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://tiny-semolina-5cec1e.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nataliia Prokopenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/NataliiaProkopenko87/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
